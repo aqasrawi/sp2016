0370d2505a89064eb9ef9f2b2780b56c567463bc
aqasrawi <aqasrawi@microsoft.com>
2016-10-24 13:43:54 -0400

    Remove Config SP

--- azuredeploy.script.ps1
+++ azuredeploy.script.ps1
@@ -1,14 +1,16 @@
-﻿param(
+﻿#param(
 
- [Parameter(Mandatory=$True)]
- [string]
- $srNumber
+ #[Parameter(Mandatory=$True)]
+ #[string]
+ #$srNumber
   
-)
+#)
 
 Login-AzureRmAccount
-Set-AzureRmContext -SubscriptionID e7bc5085-d0bf-464f-aa5c-adb09f9d8fba
+Set-AzureRmContext -SubscriptionID 3a4af7b3-b7ac-463d-9940-1d80445961a8
 
+
+$srNumber = '2016'
 $resourceGroupName = 'RG-sp' + $srNumber
 $location = 'east us'
 
@@ -29,22 +31,22 @@ adSubnet='10.0.0.0/24'
 sqlSubnet='10.0.1.0/24'
 spSubnet= '10.0.2.0/24'
 adNicIPAddress='10.0.0.4'
-adminUsername = 'ahabda'
+adminUsername = 'adiy'
 adminPassword='WSX5edc@'
 adVMSize = 'Standard_D2_v2'
 sqlVMSize = 'Standard_D2_v2'
 spVMSize='Standard_D5_V2'
-domainName= 'ahabda.local' 
-sqlServerServiceAccountUserName='ahabdasql'
+domainName= 'adiy.local' 
+sqlServerServiceAccountUserName='adiysql'
 sqlServerServiceAccountPassword='wsx5EDC@'
-sharePointSetupUserAccountUserName='ahabdasp'
+sharePointSetupUserAccountUserName='adiysp'
 sharePointSetupUserAccountPassword= 'wsx5EDC@'
 sharePointFarmAccountUserName='sp_farm'
 sharePointFarmAccountPassword= 'wsx5EDC@'
 sharePointFarmPassphrasePassword='wsx5EDC@'
 spSiteTemplateName='STS#0'
 spDNSPrefix='sp-unique'
-baseUrl='https://raw.githubusercontent.com/aayad/sharepoint-three-vm/master'
+baseUrl='https://raw.githubusercontent.com/aqasrawi/sp2016/master'
 #baseUrl = 'https://raw.githubusercontent.com/razar-msft/SharePoint-Non-HA-FARM/master'
 spPublicIPNewOrExisting='new'
 spPublicIPRGName=''
@@ -61,4 +63,4 @@ New-AzureRmResourceGroup `
 
 
 
-New-AzureRmResourceGroupDeployment -Name $deploymentName -ResourceGroupName $resourceGroupName -TemplateFile C:\Data\Work\armtemplates\azuredeploy.json -TemplateParameterObject $parameters
+New-AzureRmResourceGroupDeployment -Name $deploymentName -ResourceGroupName $resourceGroupName -TemplateFile C:\Users\aqasrawi\Documents\GitHub\sp2016\azuredeploy.json -TemplateParameterObject $parameters
