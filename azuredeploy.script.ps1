9745f5ace7d768c450db19b7dd6104e7cdfe1229
aqasrawi <aqasrawi@microsoft.com>
2016-10-28 21:24:59 -0400

    WMF5

--- azuredeploy.script.ps1
+++ azuredeploy.script.ps1
@@ -2,7 +2,7 @@
 Set-AzureRmContext -SubscriptionID 3a4af7b3-b7ac-463d-9940-1d80445961a8
 
 #$srNumber = Read-Host ("SR Number ") # 'm2016'
-$srNumber = 'v2016'
+$srNumber = 'm2016'
 $resourceGroupName = 'RGsp' + $srNumber
 $location = 'east us'
 
@@ -21,6 +21,7 @@ $sppublicIP = New-AzureRmPublicIpAddress -AllocationMethod Dynamic -ResourceGrou
 
 $parameters=@{
 
+dept = 'Finance'
 
 sharepointFarmName = $sharepointFarmName
 location ='east us'
@@ -71,9 +72,9 @@ storageAccountType='Standard_LRS'
 #
 # Provision the SP2016 VM including domain join and all pre-requisities needed for farm config
 #
-#New-AzureRmResourceGroupDeployment -Name $deploymentName -ResourceGroupName $resourceGroupName -TemplateFile $PSScriptRoot\azuredeploy-spvm.json -TemplateParameterObject $parameters -Verbose
+New-AzureRmResourceGroupDeployment -Name $deploymentName -ResourceGroupName $resourceGroupName -TemplateFile $PSScriptRoot\azuredeploy-spvm.json -TemplateParameterObject $parameters -Verbose
 
 #
 # Configure an SP farm and central admin site
 #
-New-AzureRmResourceGroupDeployment -Name $deploymentName -ResourceGroupName $resourceGroupName -TemplateFile $PSScriptRoot\azuredeploy-final-spvm.json -TemplateParameterObject $parameters -Verbose
+#New-AzureRmResourceGroupDeployment -Name $deploymentName -ResourceGroupName $resourceGroupName -TemplateFile $PSScriptRoot\azuredeploy-final-spvm.json -TemplateParameterObject $parameters -Verbose
